8639dc27484c44a19ea94091ecf4b7be542779ca
Maulid Fajar <67719670+MaulidFajar@users.noreply.github.com>
2023-05-11 16:30:36 +0700

    UTS G.111.20.0040

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import pengguna.*;
+
 class Main {
     public static void main(String[] args) {
+        Mhs mhs = new Mhs("ini_username", "ini_password");
+        mhs.setNama("Muhammad Maulid Fajar");
+        mhs.setNim("G.111.20.0040");
+        mhs.setSmt(6);
 
+        System.out.println("Nama     : " + mhs.getNama());
+        System.out.println("NIM      : " + mhs.getNim());
+        System.out.println("Smt      : " + mhs.getSmt());
+        System.out.println("Username : " + mhs.getUsername());
+        System.out.println("Password : " + mhs.getPassword());
     }
 }
